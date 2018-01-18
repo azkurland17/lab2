2124421261bca97675e28fcd33890fce23f1c796
azkurland17 <azkurlan@ucsd.edu>
2018-01-18 09:52:59 -0800

    d

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,7 +2,7 @@
 
 function projectClick(e) { 
     // prevent the page from reloading      
-    e.preventDefault();
+    //e.preventDefault();
     // In an event handler, $(this) refers to      
     // the object that triggered the event      
     $(this).css("background-color", "#7fff00");
