3790765ba0bdfbb9dbc1a8f3245d9780d10db0ef
azkurland17 <azkurlan@ucsd.edu>
2018-01-18 01:45:49 -0800

    added js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,5 +1,13 @@
 'use strict';
 
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -13,6 +21,10 @@ function initializePage() {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
 
+
+
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a.thumbnail").click(projectClick);
 }
